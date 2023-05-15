6e7f3e7306cc75a58ed88ae07e7a886e138cd2aa
nz2710 <ngoquocdung2710@gmail.com>
2023-05-15 16:52:46 +0700

    Revert "Revert "test""
    This reverts commit 514a62fd1594a3eeb4ec5fda0cac6ca7e9cfefa7.

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\JobCategory;
 use App\Models\PageHomeItem;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $home_page_data = PageHomeItem::where('id', 1)->first();
-        return view('front.home',compact('home_page_data'));
+        $job_categories = JobCategory::withCount('rJob')->orderBy('r_job_count','desc')->take(9)->get();
+        $all_job_categories = JobCategory::orderBy('name','asc')->get();
+        return view('front.home',compact('home_page_data','job_categories','all_job_categories'));
     }
 }
