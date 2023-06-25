c59ba6b0be1f1322bc9bd4ac91489b2a25bc5a4c
nz2710 <ngoquocdung2710@gmail.com>
2023-06-25 19:45:50 +0700

    Integrated Payment method for company
    Paypal and Stripe
    Show the orders of company

--- resources/views/company/sidebar.blade.php
+++ resources/views/company/sidebar.blade.php
@@ -2,13 +2,11 @@
     <li class="list-group-item {{ Request::is('company/dashboard') ? 'active' : '' }}">
         <a href="{{ route('company_dashboard') }}">Dashboard</a>
     </li>
-    <li class="list-group-item">
-        <a href="company-payment.html"
-            >Make Payment</a
-        >
+    <li class="list-group-item {{ Request::is('company/make-payment') ? 'active' : '' }}">
+        <a href="{{ route('company_make_payment') }}">Make Payment</a>
     </li>
-    <li class="list-group-item">
-        <a href="company-orders.html">Orders</a>
+    <li class="list-group-item {{ Request::is('company/orders') ? 'active' : '' }}">
+        <a href="{{ route('company_orders') }}">Orders</a>
     </li>
     <li class="list-group-item">
         <a href="company-job-add.html"
